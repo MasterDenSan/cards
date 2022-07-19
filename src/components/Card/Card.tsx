060564fb0a553f508ab4b9ee636fdda8277039ed
denislamH <lad@crtweb.ru>
2022-07-19 23:39:28 +0500

    fix Card.tsx

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -1,4 +1,4 @@
-import {memo} from 'react'
+import {FC, memo} from 'react'
 import './Card.scss'
 
 
@@ -10,12 +10,13 @@ interface ICard {
     order: number
 }
 
-export default memo(function Card({card, isRoll, index, hanleRoll, order}: ICard): JSX.Element  {
-
+const Card: FC<ICard> = ({card, isRoll, index, hanleRoll, order, ...otherProps} ): JSX.Element => {
     return (
-        <div className={isRoll ? "card roll" : "card"} onClick={() => hanleRoll(index)} style={{order: order + index}}>
+        <div className={isRoll ? "card roll" : "card"} onClick={() => hanleRoll(index)} style={{order: order + index}} {...otherProps}>
             <img className="card__front" src={`./images/${card}.svg`} alt=""/>
             <img className="card__back" src="./images/back.svg" alt=""/>
         </div>
     )
-})
+}
+
+export default memo(Card)
