93754637f8c4ddfc34c207391278d05541774a48
DenPetDev <kilshiki@rambler.ru>
2022-07-25 23:45:21 +0500

    Edded correct eslint in the console

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -4,11 +4,15 @@ import {ICard} from "./types";
 
 const Card: FC<ICard> = ({card, isRoll, index, hanleRoll, order, ...otherProps} ): JSX.Element => {
 
-
+    const myUnusedVariable = 42;
 
 
     return (
-        <div className={isRoll ? "card roll" : "card"} onClick={() => hanleRoll(index)} style={{order: order + index}} {...otherProps}>
+        <div
+            className={isRoll ? "card roll" : "card"}
+             onClick={() => hanleRoll(index)}
+             style={{order: order + index}} {...otherProps}
+        >
             <img className="card__front" src={`./images/${card}.svg`} alt=""/>
             <img className="card__back" src="./images/back.svg" alt=""/>
         </div>
--- src/index.jsx
+++ src/index.jsx
@@ -3,5 +3,6 @@ import ReactDOM from "react-dom";
 import './styles/style.scss'
 import App from "./App";
 
+const a = 1;
 
 ReactDOM.render(<App/>, document.getElementById('root'))
